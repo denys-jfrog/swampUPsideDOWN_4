ddb0b6dee3c89271443103645d9470eaf849f37a
Denys Shevchenko <denyss@jfrog.com>
2024-12-18 11:18:15 +0200

    Frog

---
+++ Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class App {
+
+    // frog Frog FROG frogs Frogs FROGS aaa
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello frog Frog FROG frogs")
+    }
+}
